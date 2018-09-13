09089071e38af9634038059dcb6e355dd44eaf3c
Sergey Vaschenko <vaschenko.sergey@gmail.com>
2018-09-13 10:23:01 +0300

    add provider response entity

---
+++ src/main/java/com/overseaslabs/examples/mailer/entity/ProviderResponse.java
@@ -0,0 +1,32 @@
+package com.overseaslabs.examples.mailer.entity;
+
+/**
+ * Mail service provider response
+ */
+public class ProviderResponse {
+    private Email email;
+    private Boolean success;
+    private String message;
+
+    ProviderResponse(Boolean success, String message) {
+        this.success = success;
+        this.message = message;
+    }
+
+    ProviderResponse(Boolean success, String message, Email email) {
+        this(success, message);
+        this.email = email;
+    }
+
+    public Email getEmail() {
+        return email;
+    }
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
